b972804bae9944e549cfd91bf864d5d82c54a8e9
Ra Mänd <ram4nd@gmail.com>
2015-07-01 17:17:51 +0300

    Update colorbox, live press breafing video fixes.

---
+++ profiles/vp_profile/modules/vp_live_press_briefings/vp_live_press_briefings.js
@@ -0,0 +1,15 @@
+(function($){$().ready(function(){
+  $('a.import-video-youtube-live').click(function(e){
+    e.preventDefault();
+    $('body').append('<div id="import-video-stream-youtube"><iframe width="600" height="332" src="//www.youtube.com/embed/' + Drupal.settings.vp_live_press_briefing + '" frameborder="0" allowfullscreen></iframe></div>');
+    $('#import-video-stream-youtube').dialog({
+      height: 407,
+      scrolling: false,
+      width: 640,
+      position: { my: "center", at: "center", of: window },
+      close: function() {
+        $(this).remove();
+      }
+    });
+  });
+});})(jQuery);
