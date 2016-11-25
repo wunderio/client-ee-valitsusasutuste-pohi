3522e3b62b828d31932bc7a1c569154563052fb3
Hannes Kirsman <hannes.kirsman@wunderkraut.com>
2016-11-25 10:10:29 +0200

    Refactor accordion script, remove ui accordion and add custom code.

--- profiles/vp_profile/themes/vp_theme/js/init_ui.accordion.js
+++ profiles/vp_profile/themes/vp_theme/js/init_ui.accordion.js
@@ -1,9 +1,27 @@
 /**
- * Init the jQuery UI Accordion script.
+ * Accordion like module.
  */
 
 (function($) {
   $().ready(function() {
-    $('DIV.template-custom-accordion').accordion();
+    var $accordion = $('DIV.template-custom-accordion'),
+        $accordionContents = $accordion.find('.ui-accordion-content'),
+        $accordionHeaders = $accordion.find('.ui-accordion-header');
+
+    // Default state - first open.
+    $accordionContents.hide();
+    $accordionContents.first().show();
+    $accordionHeaders.first().addClass('ui-accordion-header-active');
+
+    $accordionHeaders.click(function() {
+      var $next = $(this).next();
+      $next.toggle();
+      if ($next.is(':visible')) {
+        $(this).addClass('ui-accordion-header-active');
+      }
+      else {
+        $(this).removeClass('ui-accordion-header-active');
+      }
+    });
   });
 })(jQuery);
