fdd2e55ab923cf87ccf1dffa1e159f4bf6bd3d6a
Hannes Kirsman <hannes.kirsman@wunderkraut.com>
2016-12-13 15:03:29 +0200

    LA-19 Remove console.log().

--- profiles/vp_profile/modules/vp_legal_act/js/bef-tree-select-sub-checkboxes.js
+++ profiles/vp_profile/modules/vp_legal_act/js/bef-tree-select-sub-checkboxes.js
@@ -6,8 +6,6 @@
   $('UL.bef-tree').each(function() {
     var level1Checkbox = $(this).find('> ul > li > div input');
 
-    console.log(level1Checkbox.length);
-
     level1Checkbox.change(function() {
       var $checkbox = $(this);
       if ($checkbox.is(':checked') === true) {
