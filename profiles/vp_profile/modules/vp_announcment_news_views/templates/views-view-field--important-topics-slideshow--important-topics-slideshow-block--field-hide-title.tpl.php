e23675cacb0530c2894c6b89c251bc1d65a03fbf
Hannes Kirsman <hannes.kirsman@wunderkraut.com>
2017-03-29 11:20:34 +0300

    VAP-38 Add feature to hide important topics title.

---
+++ profiles/vp_profile/modules/vp_announcment_news_views/templates/views-view-field--important-topics-slideshow--important-topics-slideshow-block--field-hide-title.tpl.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @file
+ * This template is used to print a single field in a view.
+ *
+ * We use this to create css that hides title if the option is set from node.
+ */
+?>
+<?php if (isset($row->field_field_hide_title[0]['raw']['value'])): ?>
+<style>.view-important-topics-slideshow__title-<?php print $row->nid; ?> { display: none; }</style>
+<?php endif; ?>
