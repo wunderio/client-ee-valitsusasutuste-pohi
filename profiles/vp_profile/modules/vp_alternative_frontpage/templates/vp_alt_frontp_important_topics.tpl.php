0057cb2e6bcec13feb07373a67fe9b6b4c14ed12
Hannes Kirsman <hannes.kirsman@wunderkraut.com>
2017-09-13 17:07:00 +0300

    RVB-23 Theme and configure the news section for the alternative frontpage.
    Also:
    - start adding some general layout logic
    - create new news
    - Configure teaser (seems like it's not used so we use it for our new views)
      and create intro teaser for the first news.

---
+++ profiles/vp_profile/modules/vp_alternative_frontpage/templates/vp_alt_frontp_important_topics.tpl.php
@@ -0,0 +1,11 @@
+<img src="/profiles/vp_profile/modules/vp_alternative_frontpage/img/example-content/foto-aktuaalsed.jpg">
+
+<ul>
+  <li><a href="#">X-tee versiooni 5 keskkonnad sulguvad</a></li>
+  <li><a href="#">DDOC dokumendid tuleks üle tembeldada</a></li>
+</ul>
+
+<ul>
+  <li><a href="#">Dokumendivahetuskeskus lõpetab töö ja asendub dokumendivahetuskihiga</a></li>
+  <li><a href="#">RIHA uueneb</a></li>
+</ul>
