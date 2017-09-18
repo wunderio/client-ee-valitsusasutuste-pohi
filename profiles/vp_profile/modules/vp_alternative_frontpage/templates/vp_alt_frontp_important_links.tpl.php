80ba06324b15691564a7cb1071efaf94784d3d00
Hannes Kirsman <hannes.kirsman@wunderkraut.com>
2017-09-18 18:10:28 +0300

    RVB-23 Theme calendar and enable block translations.

---
+++ profiles/vp_profile/modules/vp_alternative_frontpage/templates/vp_alt_frontp_important_links.tpl.php
@@ -0,0 +1,10 @@
+<div class="vap-alternative-frontpage__static-important-links">
+  <ul>
+    <li><a href="#">Töötajate kontaktid</a></li>
+    <li><a href="#">Andmevahetuskiht X-tee</a></li>
+    <li><a href="#">Infosüsteemide turbesüsteem ISKE</a></li>
+    <li><a href="#">Turvaintsidentide käsitlemine CERT-EE</a></li>
+    <li><a href="#">Hooldusteated</a></li>
+    <li><a href="#">Tööpakkumised</a></li>
+  </ul>
+</div>
