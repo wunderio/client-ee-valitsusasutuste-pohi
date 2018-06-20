1c1f59aa3f66c62b40e33a83a618c654261f4d71
Nikita Izotov <nikita.izotov@wunder.io>
2018-06-20 15:57:11 +0300

    RVB-133: Addthis switcher added.

--- profiles/vp_profile/themes/vp_theme/templates/field--field-picture.tpl.php
+++ profiles/vp_profile/themes/vp_theme/templates/field--field-picture.tpl.php
@@ -41,8 +41,9 @@ $gallery = node_load($gallery_nids[0]);
     ?>
     <div class="field-item <?php print $delta % 2 ? 'odd' : 'even'; ?>"<?php print $item_attributes[$delta]; ?>>
 
-    <script type="text/javascript">
-       (function($){$().ready(function(){
+    <?php if (module_exists('minimal_share')): ?>
+      <script type="text/javascript">
+        (function($){$().ready(function(){
           var colorbox_<?php echo $item['#item']['fid']; ?> = $('#colorbox-<?php echo $id; ?>');
           colorbox_<?php echo $item['#item']['fid']; ?>.click(function(e){
             e.stopPropagation();
@@ -66,23 +67,69 @@ $gallery = node_load($gallery_nids[0]);
             '<div class="description2"><?php echo $date . $author; ?></div>' +
             '<?php echo (empty($title) ? '' : '<div class="caption_description">'.$title.'</div>'); ?>' +
             '<div class="download">' +
-              '<a href="<?php echo $orig_file_url; ?>"><?php
-                echo t('Download original');
+            '<a href="<?php echo $orig_file_url; ?>"><?php
+              echo t('Download original');
+              ?></a>' +
+            '</div>',
+            cbox_complete: function() {
+            }
+          });
+          /* Autoload colorbox if id is in url.*/
+          var location = document.location + '';
+          if (location.indexOf('#') !== -1) {
+            var image_id_from_url = location.split('#')[1];
+            if (image_id_from_url.indexOf('colorbox-cb-') === 0) {
+              if ('colorbox-<?php echo $id; ?>' == image_id_from_url) {
+                colorbox_<?php echo $item['#item']['fid']; ?>.colorbox({open:true,current:'{current} / {total}'});
+              }
+            }
+          }
+
+        });})(jQuery);
+      </script>
+    <?php else: ?>
+      <script type="text/javascript">
+        (function($){$().ready(function(){
+          var colorbox_<?php echo $item['#item']['fid']; ?> = $('#colorbox-<?php echo $id; ?>');
+          colorbox_<?php echo $item['#item']['fid']; ?>.click(function(e){
+            e.stopPropagation();
+            e.preventDefault();
+            if ($('body').hasClass('responsive-layout-mobile')) {
+              return false;
+            }
+            $(this).colorbox({open:true,current:'{current} / {total}'});
+          });
+          colorbox_<?php echo $item['#item']['fid']; ?>.colorbox({
+            initialHeight: Drupal.settings.colorbox.initialHeight,
+            initialWidth: Drupal.settings.colorbox.initialWidth,
+            innerWidth: $('#zone-content').width() * 0.8,
+            reposition: Drupal.settings.colorbox.reposition,
+            speed: Drupal.settings.colorbox.speed,
+            transition: Drupal.settings.colorbox.transition,
+            current: '{current} / {total}',
+            href: '<?php echo $colorbox_file_url; ?>',
+            rel: 'vp-gallery',
+            title:
+            '<div class="description2"><?php echo $date . $author; ?></div>' +
+            '<?php echo (empty($title) ? '' : '<div class="caption_description">'.$title.'</div>'); ?>' +
+            '<div class="download">' +
+            '<a href="<?php echo $orig_file_url; ?>"><?php
+              echo t('Download original');
               ?></a>' +
             '</div>',
             cbox_complete: function() {
               var $addthis_colorbox = $('#addthis_toolbox_colorbox');
               if ($addthis_colorbox.length===0) {
                 $addthis_colorbox = $('<div id="addthis_toolbox_colorbox" class="addthis_toolbox addthis_toolbox_colorbox addthis_default_style addthis_16x16_style">' +
-                '<a class="addthis_button_facebook"></a>' +
-                '<a class="addthis_button_twitter"></a>' +
-                '<a class="addthis_button_google_plusone_share"></a>' +
-                '<a class="addthis_button_linkedin"></a>' +
-              '</div>');
+                  '<a class="addthis_button_facebook"></a>' +
+                  '<a class="addthis_button_twitter"></a>' +
+                  '<a class="addthis_button_google_plusone_share"></a>' +
+                  '<a class="addthis_button_linkedin"></a>' +
+                  '</div>');
               }
               $addthis_colorbox.attr('addthis:title', '<?php echo $title; ?>');
               $addthis_colorbox.attr('addthis:url', '<?php $path = isset($_GET['q']) ? $_GET['q'] : '<front>';
-print url($path, array('absolute' => TRUE)); ?>#colorbox-<?php echo $id; ?>');
+                print url($path, array('absolute' => TRUE)); ?>#colorbox-<?php echo $id; ?>');
               $('#cboxTitle').after($addthis_colorbox);
               addthis.toolbox('#addthis_toolbox_colorbox');
 
@@ -109,7 +156,8 @@ print url($path, array('absolute' => TRUE)); ?>#colorbox-<?php echo $id; ?>');
           }
 
         });})(jQuery);
-    </script>
+      </script>
+    <?php endif; ?>
 
     <a id="colorbox-<?php echo $id; ?>" class="vp-gallery-colorbox" rel="vp-gallery" href="#">
       <img src="<?php echo $thumb_file_url; ?>" alt="<?php echo $item['#item']['alt']; ?>" />
