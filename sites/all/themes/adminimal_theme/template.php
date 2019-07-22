91c32d75f004d487a2490185e525ce6d644c9de5
Tormi Tabor <tormi.tabor@gmail.com>
2019-07-22 04:29:33 +0300

    MKMWWW-361: Update adminimal_theme.

--- sites/all/themes/adminimal_theme/template.php
+++ sites/all/themes/adminimal_theme/template.php
@@ -30,20 +30,50 @@ function adminimal_preprocess_html(&$vars) {
   drupal_add_css($adminimal_path . '/css/style.css', array('group' => CSS_THEME, 'media' => 'all', 'weight' => 1));
 
   // Add conditional CSS for IE8 and below.
-  drupal_add_css($adminimal_path . '/css/ie.css', array('group' => CSS_THEME, 'browsers' => array('IE' => 'lte IE 8', '!IE' => FALSE), 'weight' => 999, 'preprocess' => FALSE));
+  drupal_add_css($adminimal_path . '/css/ie.css', array('group' => CSS_THEME, 'browsers' => array('IE' => 'lte IE 8', '!IE' => FALSE), 'weight' => 999, 'preprocess' => TRUE));
 
   // Add conditional CSS for IE7 and below.
-  drupal_add_css($adminimal_path . '/css/ie7.css', array('group' => CSS_THEME, 'browsers' => array('IE' => 'lte IE 7', '!IE' => FALSE), 'weight' => 999, 'preprocess' => FALSE));
+  drupal_add_css($adminimal_path . '/css/ie7.css', array('group' => CSS_THEME, 'browsers' => array('IE' => 'lte IE 7', '!IE' => FALSE), 'weight' => 999, 'preprocess' => TRUE));
 
   // Add conditional CSS for IE6.
-  drupal_add_css($adminimal_path . '/css/ie6.css', array('group' => CSS_THEME, 'browsers' => array('IE' => 'lte IE 6', '!IE' => FALSE), 'weight' => 999, 'preprocess' => FALSE));
+  drupal_add_css($adminimal_path . '/css/ie6.css', array('group' => CSS_THEME, 'browsers' => array('IE' => 'lte IE 6', '!IE' => FALSE), 'weight' => 999, 'preprocess' => TRUE));
+
+  //Add Homebox module support
+  if (module_exists('homebox')) {
+    drupal_add_css($adminimal_path . '/css/homebox_custom.css', array('group' => CSS_THEME, 'media' => 'all', 'weight' => 1));
+  }
 
   // Add theme name to body class.
   $vars['classes_array'][] = 'adminimal-theme';
 
+  // Style checkbox and radio buttons in Webkit Browsers.
+  if (theme_get_setting('style_checkboxes')) {
+    $vars['classes_array'][] = 'style-checkboxes';
+  }
+
+  // Disable rounded buttons setting.
+  if (!theme_get_setting('rounded_buttons')) {
+    $vars['classes_array'][] = 'no-rounded-buttons';
+  }
+
+  // Enable sticky action buttons.
+  if (theme_get_setting('sticky_actions')) {
+    $vars['classes_array'][] = 'sticky-actions';
+  }
+
   // Add icons to the admin configuration page.
   if (theme_get_setting('display_icons_config')) {
-    drupal_add_css($adminimal_path . '/css/icons-config.css', array('group' => CSS_THEME, 'weight' => 10, 'preprocess' => FALSE));
+    drupal_add_css($adminimal_path . '/css/icons-config.css', array('group' => CSS_THEME, 'weight' => 10, 'preprocess' => TRUE));
+  }
+
+  // Add icons to the admin configuration page.
+  if (theme_get_setting('avoid_custom_font')) {
+    drupal_add_css($adminimal_path . '/css/avoid_custom_font.css', array('group' => CSS_THEME, 'weight' => 9000, 'preprocess' => TRUE));
+  }
+
+  // Load CKEditor styles if enabled in settings.
+  if (theme_get_setting('adminimal_ckeditor') and theme_get_setting('adminimal_theme_skin') != 'dark') {
+    drupal_add_css($adminimal_path . '/css/ckeditor-adminimal.css', array('group' => CSS_THEME, 'media' => 'all', 'weight' => 2));
   }
 
   // Define Default media queries.
@@ -56,14 +86,45 @@ function adminimal_preprocess_html(&$vars) {
     $media_query_tablet = theme_get_setting('media_query_tablet');
   }
 
+  // Use Bootstrap Gids.
+  if (theme_get_setting('use_bootstrap_grids')) {
+    drupal_add_css($adminimal_path . '/css/bootstrap-grids.css', array('group' => CSS_THEME, 'weight' => 1000, 'preprocess' => TRUE));
+  }
+
+  // Load custom Adminimal skin.
+  $adminimal_skin = theme_get_setting('adminimal_theme_skin');
+  if ((!is_null($adminimal_skin))) {
+    drupal_add_css($adminimal_path . '/skins/' . $adminimal_skin . '/' . $adminimal_skin . '.css', array('group' => CSS_THEME, 'weight' => 900, 'preprocess' => TRUE));
+    // Add conditional CSS for Mac OS X.
+    drupal_add_css($adminimal_path . '/skins/' . $adminimal_skin . '/mac_os_x.css', array('group' => CSS_THEME, 'weight' => 950, 'preprocess' => TRUE));
+    drupal_add_js($adminimal_path . '/skins/' . $adminimal_skin . '/' . $adminimal_skin . '.js');
+    $vars['classes_array'][] = 'adminimal-skin-' . $adminimal_skin ;
+  }
+  else {
+    drupal_add_css($adminimal_path . '/skins/default/default.css', array('group' => CSS_THEME, 'weight' => 900, 'preprocess' => TRUE));
+    // Add conditional CSS for Mac OS X.
+    drupal_add_css($adminimal_path . '/skins/default/mac_os_x.css', array('group' => CSS_THEME, 'weight' => 950, 'preprocess' => TRUE));
+    drupal_add_js($adminimal_path . '/skins/default/default.js');
+    $vars['classes_array'][] = 'adminimal-skin-default' ;
+  }
+
   // Add responsive styles.
   drupal_add_css($adminimal_path . '/css/mobile.css', array('group' => CSS_THEME, 'media' => $media_query_mobile, 'weight' => 1000));
   drupal_add_css($adminimal_path . '/css/tablet.css', array('group' => CSS_THEME, 'media' => $media_query_tablet, 'weight' => 1000));
 
   // Add custom CSS.
-  $custom_css_path = 'public://adminimal-custom.css';
-  if (theme_get_setting('custom_css') && file_exists($custom_css_path)) {
-    drupal_add_css($custom_css_path, array('group' => CSS_THEME, 'weight' => 9999, 'preprocess' => FALSE));
+  if (theme_get_setting('custom_css')) {
+    $custom_css_path = theme_get_setting('custom_css_path');
+    if (empty($custom_css_path)) {
+      $custom_css_path = 'public://adminimal-custom.css';
+    }
+    if (file_exists($custom_css_path)) {
+      drupal_add_css($custom_css_path, array(
+        'group' => CSS_THEME,
+        'weight' => 9999,
+        'preprocess' => TRUE,
+      ));
+    }
   }
 
   // Fix the viewport and zooming in mobile devices.
@@ -76,6 +137,35 @@ function adminimal_preprocess_html(&$vars) {
   );
   drupal_add_html_head($viewport, 'viewport');
 
+  // Remove the no-sidebars class which is always added by core. Core assumes
+  // the sidebar regions are called sidebar_first and sidebar_second, which
+  // is not the case in this theme.
+  $key = array_search('no-sidebars', $vars['classes_array']);
+  if ($key !== FALSE) {
+    unset($vars['classes_array'][$key]);
+  }
+  // Add information about the number of sidebars.
+  if (!empty($vars['page']['sidebar_left']) && !empty($vars['page']['sidebar_right'])) {
+    $vars['classes_array'][] = 'two-sidebars';
+  }
+  elseif (!empty($vars['page']['sidebar_left'])) {
+    $vars['classes_array'][] = 'one-sidebar sidebar-left';
+  }
+  elseif (!empty($vars['page']['sidebar_right'])) {
+    $vars['classes_array'][] = 'one-sidebar sidebar-right';
+  }
+  else {
+    $vars['classes_array'][] = 'no-sidebars';
+  }
+
+  // Display warning message on certain pages if theme is disabled.
+  if (in_array('page-admin-appearance', $vars['classes_array']) || in_array('page-admin-modules', $vars['classes_array']) || in_array('page-admin-reports-status', $vars['classes_array'])) {
+    $active_themes = list_themes();
+    if ($active_themes['adminimal']->status == 0) {
+      global $base_url;
+      drupal_set_message(t('Adminimal Theme must be enabled to work properly. Please enable it from the <a href="@link">Appearance page</a>.', array('@link' => $base_url . '/admin/appearance')), 'warning');
+    }
+  }
 }
 
 /**
@@ -88,7 +178,7 @@ function adminimal_preprocess_page(&$vars) {
     '#theme' => 'menu_local_tasks',
     '#secondary' => $vars['tabs']['#secondary'],
   );
-
+  unset($vars['page']['hidden']);
 }
 
 /**
@@ -138,7 +228,7 @@ function adminimal_adminimal_block_content($variables) {
 
 /**
  * Implements theme_tablesort_indicator().
- * 
+ *
  * Use our own image versions, so they show up as black and not gray on gray.
  */
 function adminimal_tablesort_indicator($variables) {
@@ -166,6 +256,16 @@ function adminimal_css_alter(&$css) {
   // Use Seven's jQuery UI theme style instead of the default one.
   if (isset($css['misc/ui/jquery.ui.theme.css'])) {
     $css['misc/ui/jquery.ui.theme.css']['data'] = drupal_get_path('theme', 'adminimal') . '/css/jquery.ui.theme.css';
+  }
+}
+
+/**
+ * Implements hook_js_alter().
+ */
+function adminimal_js_alter(&$javascript) {
+  // Fix module filter available updates page.
+  if (module_exists('module_filter') && isset($javascript[drupal_get_path('module', 'module_filter') . '/js/update_status.js'])) {
+    $javascript[drupal_get_path('module','module_filter').'/js/update_status.js']['data'] = drupal_get_path('theme', 'adminimal') . '/js/update_status.js';
   }
 }
 
@@ -206,7 +306,6 @@ function adminimal_admin_block($variables) {
   $output .= '</div>';
 
   return $output;
-
 }
 
 /**
@@ -251,7 +350,7 @@ function adminimal_table($variables) {
   $empty = $variables['empty'];
 
   // Add sticky headers, if applicable.
-  if (count($header) && $sticky) {
+  if (!empty($header) && $sticky) {
     drupal_add_js('misc/tableheader.js');
     // Add 'sticky-enabled' class to the table to identify it for JS.
     // This is needed to target tables constructed by this function.
@@ -266,7 +365,7 @@ function adminimal_table($variables) {
   }
 
   // Format the table columns:
-  if (count($colgroups)) {
+  if (!empty($colgroups)) {
     foreach ($colgroups as $number => $colgroup) {
       $attributes = array();
 
@@ -301,14 +400,16 @@ function adminimal_table($variables) {
   }
 
   // Add the 'empty' row message if available.
-  if (!count($rows) && $empty) {
+  if (empty($rows) && $empty) {
     $header_count = 0;
-    foreach ($header as $header_cell) {
-      if (is_array($header_cell)) {
-        $header_count += isset($header_cell['colspan']) ? $header_cell['colspan'] : 1;
-      }
-      else {
-        ++$header_count;
+    if (!empty($header)) {
+      foreach ($header as $header_cell) {
+        if (is_array($header_cell)) {
+          $header_count += isset($header_cell['colspan']) ? $header_cell['colspan'] : 1;
+        }
+        else {
+          ++$header_count;
+        }
       }
     }
     $rows[] = array(array(
@@ -319,24 +420,24 @@ function adminimal_table($variables) {
   }
 
   // Format the table header:
-  if (count($header)) {
+  if (!empty($header)) {
     $ts = tablesort_init($header);
     // HTML requires that the thead tag has tr tags in it followed by tbody
     // tags. Using ternary operator to check and see if we have any rows.
-    $output .= (count($rows) ? ' <thead><tr>' : ' <tr>');
+    $output .= (!empty($rows) ? ' <thead><tr>' : ' <tr>');
     foreach ($header as $cell) {
       $cell = tablesort_header($cell, $header, $ts);
       $output .= _theme_table_cell($cell, TRUE);
     }
     // Using ternary operator to close the tags based on whether or not there are rows
-    $output .= (count($rows) ? " </tr></thead>\n" : "</tr>\n");
+    $output .= (!empty($rows) ? " </tr></thead>\n" : "</tr>\n");
   }
   else {
     $ts = array();
   }
 
   // Format the table rows:
-  if (count($rows)) {
+  if (!empty($rows)) {
     $output .= "<tbody>\n";
     $flip = array(
       'even' => 'odd',
@@ -359,7 +460,7 @@ function adminimal_table($variables) {
         $attributes = array();
         $no_striping = FALSE;
       }
-      if (count($cells)) {
+      if (!empty($cells)) {
         // Add odd/even class
         if (!$no_striping) {
           $class = $flip[$class];
@@ -382,4 +483,19 @@ function adminimal_table($variables) {
   $output .= "</table>\n";
   $output .= "</div>\n";
   return $output;
+}
+
+/**
+ * Set dark skin for CKEditor.
+ * Implements hook_ckeditor_settings_alter().
+ */
+function adminimal_ckeditor_settings_alter(&$settings) {
+
+  // Check if selected adminimal skin is dark.
+  if (theme_get_setting('adminimal_theme_skin') == 'dark') {
+    $adminimal_path = drupal_get_path('theme', 'adminimal');
+    global $base_url;
+    $settings['skin'] = 'adminimal-dark, '. $base_url .'/'. $adminimal_path . '/skins/dark/ckeditor/';
+    array_push($settings['contentsCss'], $base_url .'/'. $adminimal_path . '/skins/dark/ckeditor/contents.css');
+  }
 }
--- sites/all/themes/adminimal_theme/theme-settings.php
+++ sites/all/themes/adminimal_theme/theme-settings.php
@@ -1,6 +1,5 @@
 <?php
 
-
 /**
  * @file
  * Theme setting callbacks for the Adminimal theme.
@@ -20,7 +19,10 @@ function adminimal_form_system_theme_settings_alter(&$form, &$form_state) {
   global $base_url;
   $adminimal_path = drupal_get_path('theme', 'adminimal');
   $old_css_path = $adminimal_path . '/css/custom.css';
-  $custom_css_path = 'public://adminimal-custom.css';
+  $custom_css_path = theme_get_setting('custom_css_path');
+  if (empty($custom_css_path)) {
+    $custom_css_path = 'public://adminimal-custom.css';
+  }
   $custom_css_dir = str_replace($base_url . '/', "", file_create_url($custom_css_path));
   $custom_css_url = file_create_url($custom_css_path);
 
@@ -49,10 +51,74 @@ function adminimal_form_system_theme_settings_alter(&$form, &$form_state) {
     '#weight' => -10,
   );
 
+  $form['skin'] = array(
+    '#type' => 'fieldset',
+    '#title' => t('Adminimal skin'),
+    '#weight' => -11,
+  );
+
+  // Create the select list.
+  $form['skin']['adminimal_theme_skin'] = array(
+    '#type' => 'select',
+    '#title' => t('Skin selection'),
+    '#default_value' => theme_get_setting('adminimal_theme_skin'),
+    '#options' => array(
+      'default' => t('Adminimal Default'),
+      'dark' => t('Adminimal Dark (BETA)'),
+      //'flat' => t('Flat'),
+      'material' => t('Material (BETA)'),
+      'alternative' => t('Alternative'),
+    ),
+    '#description' => t('Select desired skin style. Note that this feature is in beta stage and there might be some issues.'),
+    '#required' => FALSE,
+  );
+
+  $form['adminimal_custom']['use_bootstrap_grids'] = array(
+    '#type' => 'checkbox',
+    '#title' => t('Use Bootstrap Grids'),
+    '#default_value' => theme_get_setting('use_bootstrap_grids'),
+  );
+
+  $form['adminimal_custom']['style_checkboxes'] = array(
+    '#type' => 'checkbox',
+    '#title' => t('Style checkboxes and radio buttons in Webkit browsers.'),
+    '#description' => t('Enabling this option will style checkbox and radio buttons for Webkit browsers like Google Chrome, Safari, Opera and their mobile versions.
+     Enabling this option will <strong>not</strong> have any negative impact on older browsers that dont support pure CSS styling of checkboxes like Internet Explorer or Firefox.'),
+    '#default_value' => theme_get_setting('style_checkboxes'),
+  );
+
   $form['adminimal_custom']['display_icons_config'] = array(
     '#type' => 'checkbox',
     '#title' => t('Display icons in Configuration page'),
     '#default_value' => theme_get_setting('display_icons_config'),
+  );
+
+  $form['adminimal_custom']['rounded_buttons'] = array(
+    '#type' => 'checkbox',
+    '#title' => t('Use rounded buttons'),
+    '#description' => t('Uncheck this setting if you dont like the rounded button styling for some action links'),
+    '#default_value' => theme_get_setting('rounded_buttons'),
+  );
+
+  $form['adminimal_custom']['sticky_actions'] = array(
+    '#type' => 'checkbox',
+    '#title' => t('Sticky form actions'),
+    '#description' => t('This will make the form actions div fixed bottom positioning. So for example when you visit the node edit page you wont need to scroll down to save/preview/delete the node. The form action buttons will be sticky to the bottom of the screen.'),
+    '#default_value' => theme_get_setting('sticky_actions'),
+  );
+
+  $form['adminimal_custom']['avoid_custom_font'] = array(
+    '#type' => 'checkbox',
+    '#title' => t('Avoid using "Open Sans" font'),
+    '#description' => t('(useful for languages that are not well supported by the "Open sans" font. Like Japanese for example)'),
+    '#default_value' => theme_get_setting('avoid_custom_font'),
+  );
+
+  $form['adminimal_custom']['adminimal_ckeditor'] = array(
+    '#type' => 'checkbox',
+    '#title' => t('CKEditor support'),
+    '#description' => t('Loads custom adminimal css skin for CKEditor. Disable this to avoid css conflicts when using other CKEditor skins.'),
+    '#default_value' => theme_get_setting('adminimal_ckeditor'),
   );
 
   $form['adminimal_custom']['use_custom_media_queries'] = array(
@@ -89,35 +155,43 @@ function adminimal_form_system_theme_settings_alter(&$form, &$form_state) {
 
   $form['adminimal_custom']['custom_css'] = array(
     '#type' => 'checkbox',
-    '#title' => t('Use "adminimal-custom.css"'),
-    '#description' => t('Include adminimal-custom.css file to override or add custom css code without subthememing/hacking Adminimal Theme.'),
+    '#title' => t('Extend Adminimal with Custom CSS.'),
+    '#description' => t('Include custom css file to override or add custom css code without subthememing/hacking Adminimal Theme.'),
     '#default_value' => theme_get_setting('custom_css'),
   );
 
-  $form['adminimal_custom']['adminimal_custom_check'] = array(
+  $form['adminimal_custom']['adminimal_custom'] = array(
     '#type' => 'fieldset',
-    '#title' => t('Custom CSS file check'),
+    '#title' => t('Custom CSS file'),
+    '#description' => t('The file will be created, if not found, after saving.'),
     '#weight' => 50,
     '#states' => array(
-      // Hide the settings when the cancel notify checkbox is disabled.
+      // Hide the settings when the custom css checkbox is disabled.
       'visible' => array(
-       ':input[name="custom_css"]' => array('checked' => TRUE),
+        ':input[name="custom_css"]' => array('checked' => TRUE),
       ),
-     ),
+    ),
   );
 
   if (file_exists($custom_css_path)) {
-    $form['adminimal_custom']['adminimal_custom_check']['custom_css_description'] = array(
-      '#markup' => t('Custom CSS file Found in: !css', array('!css' => "<span class='css_path'>" . $custom_css_dir . "</span>")),
+    $form['adminimal_custom']['adminimal_custom']['custom_css_description'] = array(
+      '#markup' => t('Custom CSS file found in: !css', array('!css' => '<span class="css_path">' . $custom_css_dir . '</span>')),
       '#prefix' => '<div class="messages status custom_css_found">',
       '#suffix' => '</div>',
     );
   }
   else {
-    $form['adminimal_custom']['adminimal_custom_check']['custom_css_not_found'] = array(
-      '#markup' => t('Custom CSS file not found. You must create the !css file manually.', array('!css' => "<span class='css_path'>" . $custom_css_dir . "</span>")),
+    $form['adminimal_custom']['adminimal_custom']['custom_css_not_found'] = array(
+      '#markup' => t('Custom CSS file not found. You must create the !css file manually.', array('!css' => '<span class="css_path">' . $custom_css_dir . '</span>')),
       '#prefix' => '<div class="messages error custom_css_not_found">',
       '#suffix' => '</div>',
     );
   }
+
+  $form['adminimal_custom']['adminimal_custom']['custom_css_path'] = array(
+    '#type' => 'textfield',
+    '#title' => t('Custom CSS path'),
+    '#description' => t('<b>Leave blank to use the default path (public://adminimal-custom.css)</b>. Can be a filepath of URI'),
+    '#default_value' => theme_get_setting('custom_css_path'),
+  );
 }
